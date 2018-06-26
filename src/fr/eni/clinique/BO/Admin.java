886d5a455f8189205e6f22a60448c9349610a354
Patrick Morel <delasalle.sio.morel.p@gmail.com>
2018-06-26 10:29:28 +0200

    Add Admin.java

---
+++ src/fr/eni/clinique/BO/Admin.java
@@ -0,0 +1,19 @@
+package fr.eni.clinique.BO;
+
+public class Admin extends Personnel {
+
+
+    public Admin() {
+    }
+
+    public Admin(int codePersonnel, String nom, String motPasse, String role, boolean archive) {
+        super(codePersonnel, nom, motPasse, role, archive);
+    }
+
+    public Admin(String nom, String motPasse, String role, boolean archive) {
+        super(nom, motPasse, role, archive);
+    }
+
+
+
+}
